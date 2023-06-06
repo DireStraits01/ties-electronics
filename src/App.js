48465a07beab74d5667eaa7e3fcc38008b61a271
DireStraits01 <arsamak88@gmail.com>
2023-06-07 02:21:14 +0300

    add routers:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import React from 'react';
 import Navbar from './components/Layout/Navbar';
+import CellPhones from './components/Pages/CellPhones';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<CellPhones />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Pages/CellPhones.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CellPhones() {
+  return (
+    <div>
+      <h1>CellPhone</h1>
+    </div>
+  );
+}
+
+export default CellPhones;
